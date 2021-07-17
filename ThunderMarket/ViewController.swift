6f71cde063c0e4b82485e5791ff17819bd516c25
starpro123 <starpro123@naver.com>
2021-07-17 17:18:18 +0900

    setting: 프로젝트 생성

---
+++ ThunderMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThunderMarket
+//
+//  Created by 윤상진 on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
