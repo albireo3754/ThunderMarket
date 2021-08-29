82f270aaff1d1086f943936b6d419df125374cc9
starpro123 <starpro123@naver.com>
2021-08-29 18:54:26 +0900

    refactor: FindAddressViewController 구조 개선

--- ThunderMarket/FindAddressViewController.swift
+++ ThunderMarket/FindAddressViewController.swift
@@ -8,13 +8,54 @@
 import UIKit
 import CoreLocation
 
-class FindAddressViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
-    var isLoading = false
-    var list: [String]!
-    var nearTownCells: Town! = nil
-    var locationManager: CLLocationManager!
-    var activityIndicator: UIActivityIndicatorView!
+class FindAddressViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    private var isLoading = false
+    private var list: [String]!
+    private var nearTownCells: Town! = nil
+    private var locationManager: CLLocationManager!
+    private var activityIndicator: UIActivityIndicatorView!
+    
     @IBOutlet weak var mapTableView: UITableView!
+    
+    override func viewDidLoad() {
+        let jsonDecoder = JSONDecoder()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        renderActivityIndicator()
+        list = []
+        findAddressByCurrentLocation()
+        super.viewDidLoad()
+    }
+    
+    private func renderActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView()
+        activityIndicator.center = self.view.center
+        activityIndicator.color = .systemYellow
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.style = .large
+        self.view.addSubview(activityIndicator)
+    }
+    
+    func findAddressByCurrentLocation() {
+        self.locationManager.startUpdatingLocation()
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func clickToFindAddressByCurrentLocation(_ sender: Any) {
+        findAddressByCurrentLocation()
+    }
+
+    @IBAction func popView(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "근처 동네"
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
@@ -26,36 +67,40 @@ class FindAddressViewController: UIViewController, UITableViewDataSource, UITabl
         self.isLoading = false
         return cell
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollPosition = scrollView.contentOffset.y
+        // TODO: - frame이 뭔데?
+        let y = mapTableView.contentSize.height - scrollView.frame.height
+        print(scrollPosition, y)
+        if y - scrollPosition < CGFloat(150) && !self.isLoading {
+            self.isLoading = true
+            list = nearTownCells?.searchNear()
+            mapTableView.reloadData()
+        }
+    }
 
-    override func viewDidLoad() {
-        let jsonDecoder = JSONDecoder()
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        renderActivityIndicator()
-        super.viewDidLoad()
+}
+
+extension FindAddressViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        guard let nearTownCells = Town(point: (x: location.coordinate.longitude, y: location.coordinate.latitude)) else {
+            fatalError("data load 실패");
+        }
+        self.nearTownCells = nearTownCells
+        self.list = nearTownCells.searchNear()
+        locationManager.stopUpdatingLocation()
+        activityIndicator.stopAnimating()
+        mapTableView.reloadData()
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
-    
-    func renderActivityIndicator() {
-        activityIndicator = UIActivityIndicatorView()
-        activityIndicator.center = self.view.center
-        activityIndicator.color = .systemYellow
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.style = .large
-        activityIndicator.startAnimating()
-        self.view.addSubview(activityIndicator)
-    }
-    
-        
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
     switch status {
 
     case .authorizedAlways, .authorizedWhenInUse:
         print("GPS 권한 설정됨")
-        self.locationManager.startUpdatingLocation() // 중요!
+        self.locationManager.startUpdatingLocation()
     case .restricted, .notDetermined:
         print("GPS 권한 설정되지 않음")
         getLocationUsagePermission()
@@ -67,10 +112,6 @@ class FindAddressViewController: UIViewController, UITableViewDataSource, UITabl
     }
     }
     
-    @IBAction func findAddressByCurrentLocation(_ sender: Any) {
-        self.locationManager.startUpdatingLocation()
-        activityIndicator.startAnimating()
-    }
     func getLocationUsagePermission() {
         self.locationManager.requestWhenInUseAuthorization()
     }
@@ -85,43 +126,6 @@ class FindAddressViewController: UIViewController, UITableViewDataSource, UITabl
         alert.addAction(cancel)
         alert.addAction(setting)
         present(alert, animated: true, completion: nil)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations[locations.count - 1]
-        guard let nearTownCells = Town(point: (x: location.coordinate.longitude, y: location.coordinate.latitude)) else {
-            fatalError("data load 실패");
-        }
-        self.nearTownCells = nearTownCells
-        self.list = nearTownCells.searchNear()
-        locationManager.stopUpdatingLocation()
-        activityIndicator.stopAnimating()
-        mapTableView.reloadData()
-    }
-    
-    @IBAction func popView(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "근처 동네"
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // MARK: - ScrollView
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let scrollPosition = scrollView.contentOffset.y
-        // TODO: - frame이 뭔데?
-        let y = mapTableView.contentSize.height - scrollView.frame.height
-        print(scrollPosition, y)
-        if y - scrollPosition < CGFloat(150) && !self.isLoading {
-            self.isLoading = true
-            list = nearTownCells?.searchNear()
-            mapTableView.reloadData()
-        }
     }
 
 }
