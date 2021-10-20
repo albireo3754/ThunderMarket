21f4f8992f45087f81c792043c1735bea5ce2b87
starpro123 <starpro123@naver.com>
2021-10-20 21:59:31 +0900

    feature: TownListUseCase와 Repository추가

---
+++ ThunderMarket/Models/Repository.swift
@@ -0,0 +1,16 @@
+//
+//  Repository.swift
+//  ThunderMarket
+//
+//  Created by 윤상진 on 2021/10/20.
+//
+
+import Foundation
+import Combine
+
+protocol LocalRepository {
+}
+
+class MapRepository: LocalRepository {
+    
+}
---
+++ ThunderMarket/Models/TownListUseCase.swift
@@ -0,0 +1,12 @@
+//
+//  TownListUseCase.swift
+//  ThunderMarket
+//
+//  Created by 윤상진 on 2021/10/11.
+//
+
+import Foundation
+
+class TownListUseCase {
+    
+}
